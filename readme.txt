4e766e2c1ddaefa7d66b900058841dfc3280e9cd
joezhuu <30644138+joezhuu@users.noreply.github.com>
2018-10-23 09:16:16 +1300

    Replace FB messenger with pushover

--- readme.txt
+++ readme.txt
@@ -1,6 +1,6 @@
 1. install python
-2. run setup.bat to install praw library(reddit) and fbchat library (facebook messenger)
+2. run setup.bat to install praw library(reddit) and google.cloud library (image recognition)
 3. copy praw.ini to python_path\Lib\site-packages\praw (e.g. C:\Users\joez\AppData\Local\Programs\Python\Python37\Lib\site-packages\praw\)
 4. modify client_id and client_secret in praw.ini(this can be found at https://www.reddit.com/prefs/apps/)
-5. modify settings.json for facebook credentials and subreddit list
-6. run run.bat
+5. modify settings.json for pushover token and subreddit list
+6. run python /ReditBot.py
--- redditBot.py
+++ redditBot.py
@@ -1,65 +1,107 @@
 #!/usr/bin/python
 import praw
 import time
-from datetime import datetime
-from fbchat import Client
-from fbchat.models import *
+import datetime
 import json
 import msvcrt
+import io
+import os
+import re
+import urllib
+import requests
 
-print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+from PIL import Image
+from google.cloud import vision
+from google.cloud.vision import types
+
+print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
 # get facebook username, passwor
 with open('settings.json') as f:
     settings = json.load(f)
 
 # get known images
-hist = []
+postHist = []
 with open("hist.txt", "r") as f:
   for line in f:
-    hist.append(line.strip())
+    postHist.append(line.strip())
 
 reddit = praw.Reddit('bot1')
-client = Client(settings["botFbUser"], settings["botFbPassword"])
-imageList = hist[-20:] # keep the last 20 lines in the history
+vision_client = vision.ImageAnnotatorClient()
 
-def checkSubReddit(name, reddit, client, imageList):
+postHist = postHist[-20:] # keep the last 20 lines in the history
+
+def getCodes(imageUrl):
+    codes = []
+    file = io.BytesIO(urllib.request.urlopen(imageUrl).read())
+    content = file.read()
+
+    image = types.Image(content=content)
+
+    response = vision_client.text_detection(image=image)
+    texts = response.text_annotations
+
+    for text in texts:
+        desc = format(text.description)
+        pattern = r"\w*-\w*-\w*-\w*"
+        if re.match(pattern, desc):
+            codes.append(desc)
+
+    return codes
+
+def checkSubReddit(name, reddit, client, postHist):
     subreddit = reddit.subreddit(name)
     msg = ""
     for submission in subreddit.new(limit=3):
-        created = datetime.fromtimestamp(submission.created_utc).strftime('%Y-%m-%d %H:%M:%S')
-        title = submission.title
-        shortLink = "https://redd.it/"+submission.id
-        image = submission.url
+        created = datetime.datetime.fromtimestamp(submission.created_utc).strftime('%Y-%m-%d %H:%M:%S')
 
-        if image.endswith("png") or image.endswith("jpg"):
-            if not image in imageList:
-                print("[***NEW***] "+created+" "+title+" "+image)
+        if datetime.datetime.fromtimestamp(submission.created_utc) > datetime.datetime.now() - datetime.timedelta(days = 1, hours=0, minutes=0, seconds=0):
+        # if 1>0:
 
-                imageList.append(image)
-                user = client.searchForUsers(settings["myFbId"])[0]
-                client.sendRemoteImage(image, message=Message(text=title+"\n"+shortLink), thread_id=user.uid, thread_type=ThreadType.USER)
-            else:
-                print("[OLD] "+created+" "+title+" "+image)
+            if not submission.id in postHist:
+
+                title = submission.title
+                shortLink = "https://redd.it/"+submission.id
+                imageUrl = submission.url
+
+                print("["+created+"] "+title+" "+imageUrl)
+                postHist.append(submission.id)
+
+                if imageUrl.endswith("jpg"):
+                    
+                    if len(getCodes(imageUrl)) > 0:
+                        messageText = title+"\n"+shortLink
+                        for code in getCodes(imageUrl):
+                            messageText = messageText +"\n"+code
+
+                        print(messageText)
+                        
+                        requests.post("https://api.pushover.net/1/messages.json", data = {
+                        "token": settings["token"],
+                        "user": settings["user"],
+                        "message": messageText
+                        },
+                        files = {
+                        "attachment": ("image.jpg", io.BytesIO(urllib.request.urlopen(imageUrl).read()), "image/jpeg")
+                        }
+                        )
 
     time.sleep(20)
-    return imageList
+    return postHist
 
 
 try:	
     while 1:
         for name in settings["subreddit"]:
-            imageList = checkSubReddit(name, reddit, client, imageList)
+            postHist = checkSubReddit(name, reddit, "", postHist)
     
         time.sleep(60)
 except KeyboardInterrupt: # Ctrl+C in command line
-    client.logout()
-    print("Logout successful")
 
     with open("hist.txt", "w") as f:
-        for line in imageList:
+        for line in postHist:
             f.write(line+"\n")
     print("History updated")
-    print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
     
